36d0d79c52133aab44b144bbe723a93dc44f82fc
Tales <tabocu@gmail.com>
2018-10-26 14:49:06 -0300

    Created server and parser

---
+++ util.js
@@ -0,0 +1,81 @@
+var glpk = require("./node_modules/glpk.js");
+
+var res = {
+  name: "Food Waste",
+  time: 0.116,
+  result: {
+    vars: {
+      "Receita A": 0.6,
+      "Receita B": 0.2
+  },
+    z: 0.45999999999999996,
+    status: 5
+  }
+}
+
+function getVars(json) {
+  let vars = [];
+  let jsonKeys = Object.keys(json);
+  for (let key of jsonKeys) {
+    let item = { 
+      name: key,
+      coef: json[key]
+    }
+    vars.push(item);
+  }
+  return vars;
+}
+
+function getObjective(json) {
+  var objective = {
+    direction: glpk.GLP_MAX,
+    name: 'objetiva',
+    vars: getVars(json)
+  };
+  return objective;
+}
+
+function getBounds(max) {
+  let bnds = {
+    type: glpk.GLP_UP,
+    ub: max,
+    lb: 0.0,
+  };
+  return bnds;
+}
+
+function getSubjectTo(json) {
+  var subjectTo = [];
+  let jsonKeys = Object.keys(json); 
+  for (let key of jsonKeys) {
+    let value = json[key];
+    let item = {
+      name: key,
+      vars: getVars(value.receitas), 
+      bnds: getBounds(value.max), 
+    }
+    subjectTo.push(item);
+  }
+  return subjectTo;
+}
+
+module.exports = {
+  getGlpkFormatt: function(json) {
+    let result = {
+      name: 'Food Waste',
+      objective: getObjective(json.objetiva),
+      subjectTo: getSubjectTo(json.restricoes),
+    }
+    return result;
+  },
+
+  getFoodWasteFormat: function(json) {
+    let result = {
+      tempo: json.time,
+      quantidades: json.result.vars,
+      lucro: json.result.z,
+      status: json.result.status 
+    };
+    return result;
+  }
+}
